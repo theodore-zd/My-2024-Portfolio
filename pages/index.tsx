bdea2ae6a6f33c0e75a1ca7a72deb7fb764c94dd
Theodore Zurek-Dunne <99theodore@gmail.com>
2022-05-14 14:08:25 -0400

    updated resume fixed spelling mistake.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,18 +5,18 @@ import Home from '../page_contents/home'
 const homePage: NextPage = () => {
   return <div>
     <Head>
-      <title>Thodore Zurek-Dunne</title>
+      <title>Theodore Zurek-Dunne</title>
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
-      <meta name="description" content="Thodore Zurek-Dunne"/>
+      <meta name="description" content="Theodore Zurek-Dunne"/>
       <meta name="keywords" content=''/>
       {/* Social Embed */}
-      <meta property="og:title" content='Thodore Zurek-Dunne'/>
-      <meta property="og:description" content='Thodore Zurek-Dunne'/>
+      <meta property="og:title" content='Theodore Zurek-Dunne'/>
+      <meta property="og:description" content='Theodore Zurek-Dunne'/>
       <meta property="og:type" content="website" />
       <meta property="og:image" content='/kato_studio_preview_image.jpg' />
       <meta property="og:url" content='https://kato.studio/'/>
       <meta name="twitter:card" content="summary_large_image"/>
-      <meta property="og:site_name" content="Thodore Zurek-Dunne"/>
+      <meta property="og:site_name" content="Theodore Zurek-Dunne"/>
     </Head>
     <Home />
   </div>
