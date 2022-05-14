5f2aea6da0060067f00e78a4c5784fbe5e481df9
Theodore Zurek-Dunne <99theodore@gmail.com>
2022-05-14 13:39:25 -0400

    updated expreience bullet points to be less wordy

--- page_contents/home/experience/experience.json.ts
+++ page_contents/home/experience/experience.json.ts
@@ -3,9 +3,9 @@ export default [
     title: 'Kato.Studio',
     desc: 'Toronto, ON | Oct 2020 - May 2022',
     info: [
-      'Built a CMS to help manage client content and facilitate additional functionality for my clients. The CMS is built using Nodejs+Typescript using docker containers to allow for a microservice architecture operating headless CMS that manages content, that can be fetched from anywhere.',
-      'Many of my clients found google analytics too complicated so I built a simple analytics system, that can monitor active users on a site time spent on the page and events like mouse clicks as well as custom events and scroll percentage.',
-      'Design and develop websites for clients, ensuring good UI/UX, SEO and accessibility, as well as achieving 95+ lighthouse scores.'
+      'Built a CMS to help manage client content and facilitate additional functionality for my clients. The CMS is built using Nodejs+Typescript and Docker allowing for a microservice architecture. That operates headlessly allowing content to be fetched from anywhere.',
+      'Many of my clients found google analytics too complicated so I built a simple analytics system. that monitors active users on a site time spent on the page and events like mouse clicks as well as custom events and scroll percentage.',
+      'Designed and developed websites for clients ensuring good UI/UX, SEO and accessibility, as well as achieving 95+ lighthouse scores.'
     ]
   },
   {
@@ -19,16 +19,16 @@ export default [
     title: 'SalesGrowth Development Inc ',
     desc: 'Toronto, ON | Dec 2018 - Dec 2019',
     info: [
-      'Led redesign of an application that a client was unhappy with, ultimately retaining the client and drastically increasing client satisfaction, maintained and improved the usability of existing features.',
-      'Responsible for the design and development of 4 client sites, improving accessibility and SEO for existing sites.',
-      'Performance Improvements decreased loading times of client sites by 20% on average.'
+      'Led the redesign of an application that a client was unhappy with, ultimately retaining the client. and drastically increasing client satisfaction with the application.',
+      'Maintained and improved the usability of existing client applications.',
+      'Responsible for the design and development of 4 client sites, optimizing SEO, accessibility and performance. resulting in decreased loading times of client sites by 20% on average.'
     ]
   },
   {
     title: 'Freelance',
     desc: 'Toronto, ON | Jan 2016 - Dec 2018',
     info: [
-      'Consulted with clients on the most appropriate solution for their business\'s pain points.',
+      'Consulted with clients on the most appropriate solutions for their business pain points.',
       'Designed and implemented the given solution, i.e (rebranding, packaging, marketing materials, website redesign) resulting in increased brand recognition and sales.'
     ]
   }
