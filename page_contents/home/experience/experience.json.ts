8bd386432c75a058ffdfd0df6618b923eb9a05e2
Theodore Zurek-Dunne <99theodore@gmail.com>
2022-05-21 10:25:29 -0400

    updated resume

--- page_contents/home/experience/experience.json.ts
+++ page_contents/home/experience/experience.json.ts
@@ -3,8 +3,8 @@ export default [
     title: 'Kato.Studio',
     desc: 'Toronto, ON | Oct 2020 - May 2022',
     info: [
-      'Work with clients to design and develop websites to increase sales and customer retention. By optimizing UI/UX, SEO, and accessibility, we consistently achieved 95+ lighthouse scores.',
-      'With an in-house built CMS, we manage client content and facilitate additional functionality such as blogging systems, analytics, and editable api-first content structures. The CMS is built using NodeJs, TypeScript and Docker allowing for a microservice architecture that facilitates api-first content systems operating headlessly.',
+      'Work with clients to design and develop websites to increase sales and customer retention. By optimizing UI/UX, SEO, and accessibility, I consistently achieved 95+ lighthouse scores.',
+      'With an in-house built CMS, I manage client content and facilitate additional functionality such as blogging systems, analytics, and editable api-first content structures. The CMS is built using NodeJs, TypeScript and Docker allowing for a microservice architecture that facilitates api-first content systems operating headlessly.',
       'Many of my clients find google analytics too complicated.  I built a simple analytics system that monitors active users, time spent on the page, mouse clicks, and scroll percentage.'
     ]
   },
@@ -20,7 +20,7 @@ export default [
     desc: 'Toronto, ON | Dec 2018 - Dec 2019',
     info: [
       'Responsible for the design and development of 4 client sites, optimizing SEO, accessibility and performance. Results included decreasing load times by 20% on average.',
-      'Led the complete redesign of an application for an unhappy client.  Following 12 weeks of collaboration and development, the new design and functionality resulted in -adoption by staff and ultimately retaining the client.',
+      'Led the complete redesign of an application for an unhappy client.  Following 12 weeks of collaboration and development, the new design and functionality resulted in adoption by staff and ultimately retaining the client.',
       'Maintained and improved the usability of existing client applications.'
     ]
   },
