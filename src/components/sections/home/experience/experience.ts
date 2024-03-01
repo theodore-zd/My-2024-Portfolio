de06636b77ef01c3f3e3d4725c1f42f8a3d2d8e6
Theodore <99theodore@gmail.com>
2024-03-01 11:51:15 -0500

    💪 Updated work experience

--- src/components/sections/home/experience/experience.ts
+++ src/components/sections/home/experience/experience.ts
@@ -4,10 +4,10 @@ const experiences = [
     desc: "Full-Stack Developer, UI/UX Designer",
     info: [
       "May 2022 - Present",
-      "✦ Spearheaded a transformative UI/UX overhaul for onboarding and core systems, leading to a significant surge in user testing scores.",
-      "✦ Pioneered the integration of design systems and reusable components during the development of new features. This initiative not only accelerated development speed but also mitigated technical debt incurred from various features.",
-      "✦ Helped ship DocuSign integration, streamlining document processes and enhancing user interactions.",
-      "✦ Played a pivotal role in ensuring SOC 2 compliance, fostering a secure and trustworthy environment for data management.",
+      "✦ Led a transformative UI/UX overhaul, resulting in a significant surge in user testing scores.",
+      "✦ Spearheaded the integration of design systems and reusable components, accelerating development speed and reducing technical debt.",
+      "✦ Contributed to the successful integration of DocuSign, streamlining document processes and enhancing user interactions.",
+      "✦ Ensured SOC 2 compliance, fostering a secure and trustworthy environment for data management.",
     ],
   },
   {
@@ -15,10 +15,9 @@ const experiences = [
     desc: "Founder",
     info: [
       "October 2020 - July 2021 (10 months)",
-      "✦ Collaborated with clients to craft and enhance websites, strategically boosting sales and fostering customer retention. Achieving consistent 95+ Lighthouse scores, I prioritized optimizing UI/UX, SEO, and accessibility.",
+      "✦ Collaborated with clients to craft and enhance websites, strategically boosting sales and fostering customer retention. Achieving consistent 95+ Lighthouse scores, prioritized optimizing UI/UX, SEO, and accessibility.",
       "✦ Utilized a custom-built CMS to efficiently manage client content and introduce additional functionalities such as blogging systems, analytics, and editable API-first content structures. This microservices-based architecture, developed using NodeJs, TypeScript, and Docker, enables a seamless operation of API-first content systems in a headless mode.",
       "✦ Addressed the complexity of Google Analytics for clients by crafting a straightforward analytics system. This system monitors active users, time spent on the page, mouse clicks, and scroll percentages, providing valuable insights without unnecessary complexity.",
-      "✦ Stayed at the forefront of industry trends by actively researching and implementing emerging technologies, providing clients with innovative solutions.",
     ],
   },
   {
@@ -36,7 +35,7 @@ const experiences = [
     desc: "Frontend Engineer, UI/UX Designer",
     info: [
       "December 2018 - December 2019 (1 year 1 month)",
-      "✦ Oversaw the design and development of four client websites, focusing on optimizing SEO, enhancing accessibility, and improving performance. This effort resulted in an impressive average decrease of 20% in load times.",
+      "✦ Oversaw the design and development of 8 client websites, focusing on optimizing SEO, enhancing accessibility, and improving performance. This effort resulted in an impressive average decrease of 20% in load times.",
       "✦ Spearheaded a comprehensive redesign of an application for a dissatisfied client. Over 12 weeks of collaborative development, the revamped design and functionality not only garnered adoption by the staff but also successfully retained the client.",
       "✦ Ensured the ongoing enhancement of usability in existing client applications, contributing to sustained improvement and seamless user experiences.",
     ],
@@ -47,7 +46,7 @@ const experiences = [
     info: [
       "January 2016 - December 2018 (3 years)",
       "✦ Collaborated closely with clients to identify optimal solutions for their business challenges.",
-      "✦ Crafted and executed tailored solutions, encompassing rebranding, packaging, marketing materials, and website redesign. The outcomes were marked by heightened brand recognition and increased sales.",
+      "✦ Crafted and executed tailored solutions encompassing rebranding, packaging, marketing materials, and website redesign. The outcomes were marked by heightened brand recognition and increased sales.",
     ],
   },
 ];
