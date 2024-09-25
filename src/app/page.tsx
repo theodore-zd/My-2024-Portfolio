76511fa2a15878b186190df471aee03b562d10db
Theodore Zurek-Dunne <99theodore@gmail.com>
2024-09-25 15:44:49 -0400

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,9 @@ import HomeAboutSection from "@/components/sections/home/about";
 import HomeContactSection from "@/components/sections/home/contact";
 import HomeExperienceSection from "@/components/sections/home/experience";
 import HomeLandingSection from "@/components/sections/home/landing";
-import HomeProjectsSection from "@/components/sections/home/projects";
 import HomeSkillsSection from "@/components/sections/home/skills";
 import { HomeStarWarsSection } from "@/components/sections/home/starwars";
-import type { NextPage } from "next";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2 md:p-12 lg:p-24">
